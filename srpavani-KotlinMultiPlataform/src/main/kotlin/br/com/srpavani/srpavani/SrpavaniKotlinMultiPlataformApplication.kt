24e4171d7cfd4494c2fb8df9a5147128228e73ab
Diogo Pavani <pavanidiogo1@gmail.com>
2024-11-22 20:12:04 -0300

    first

---
+++ srpavani-KotlinMultiPlataform/src/main/kotlin/br/com/srpavani/srpavani/SrpavaniKotlinMultiPlataformApplication.kt
@@ -0,0 +1,11 @@
+package br.com.srpavani.srpavani
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SrpavaniKotlinMultiPlataformApplication
+
+fun main(args: Array<String>) {
+	runApplication<SrpavaniKotlinMultiPlataformApplication>(*args)
+}
