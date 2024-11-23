078ef4eadcf9ab388f836e39a49313786df5c2cc
Diogo Pavani <pavanidiogo1@gmail.com>
2024-11-22 23:26:22 -0300

    function send msg error

---
+++ srpavani-KotlinMultiPlataform/src/main/kotlin/br/com/srpavani/srpavani/exceptions/ExceptionResponse.kt
@@ -0,0 +1,11 @@
+package br.com.srpavani.srpavani.exceptions
+
+import java.util.*
+
+class ExceptionResponse(
+    val timeStamp: Date,
+    val message: String?,
+    val details: String?
+    ){
+}
+
