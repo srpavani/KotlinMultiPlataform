8713769b1ead97f9aeecf5df525a5095f50d316c
Diogo Pavani <pavanidiogo1@gmail.com>
2024-11-22 21:27:23 -0300

    :hammer: add rota do get

---
+++ srpavani-KotlinMultiPlataform/src/main/kotlin/br/com/srpavani/srpavani/Greeting.kt
@@ -0,0 +1,3 @@
+package br.com.srpavani.srpavani
+
+class Greeting (val id: Long, val content: String)
---
+++ srpavani-KotlinMultiPlataform/src/main/kotlin/br/com/srpavani/srpavani/GreetingController.kt
@@ -0,0 +1,15 @@
+package br.com.srpavani.srpavani
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicLong
+
+@RestController
+class GreetingController {
+    val counter: AtomicLong = AtomicLong()
+
+    @RequestMapping("/greeting")
+    fun greeting(@RequestParam(value="name", defaultValue = "world") name: String?): Greeting {
+        return Greeting(counter.incrementAndGet(), "Hello, $name!")
+    }
+}
