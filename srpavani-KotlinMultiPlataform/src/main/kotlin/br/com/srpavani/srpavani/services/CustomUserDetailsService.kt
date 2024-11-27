60585d8a675cc2818544f8c2f1a33fb60ac2d067
Diogo Pavani <pavanidiogo1@gmail.com>
2024-11-27 20:58:37 -0300

    add user details

---
+++ srpavani-KotlinMultiPlataform/src/main/kotlin/br/com/srpavani/srpavani/services/CustomUserDetailsService.kt
@@ -0,0 +1,28 @@
+package br.com.srpavani.srpavani.services
+
+import br.com.srpavani.srpavani.repository.UserRepository
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
+
+typealias ApplicationUser = br.com.srpavani.srpavani.model.User
+
+
+
+
+@Service
+class CustomUserDetailsService (private val userRepository: UserRepository): UserDetailsService {
+    override fun loadUserByUsername(username: String): UserDetails =
+        userRepository.findByEmail(username)
+            ?.mapToUserDetails()
+            ?: throw UsernameNotFoundException("Not Found")
+
+    private fun ApplicationUser.mapToUserDetails(): UserDetails =
+        User.builder()
+            .username(this.email)
+            .password(this.password)
+            .roles(this.role.name)
+            .build()
+}
